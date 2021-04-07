ac9a581df5f0b4f23660911456ab64ab584f47be
Anthony Putignano <anthony.putignano@games24x7.com>
2021-04-07 08:54:50 -0400

    Update SDK

--- SnapyrSdk.js
+++ SnapyrSdk.js
@@ -1,42 +1,57 @@
 function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 }
 
-import { orgId, workspaceId, userId, endpoint } from './vars';
+import { sdkWriteKey, channelId, userId, endpoint } from './vars';
+
+const Buffer = require('buffer').Buffer;
 
 export const track = (eventName, properties) => {
-
   const now = new Date().toISOString();
 
   const eventToSend = {
-    "messageId": uuidv4(),
-    "sentAt": now,
-    "batch": [
+    messageId: uuidv4(),
+    sentAt: now,
+    batch: [
       {
-        "type": "track",
-        "messageId": uuidv4(),
-        "userId": userId,
-        "event": eventName,
-        "timestamp": now,
-        "context": {
-          "library": {
-            "name": "slot-machine"
+        type: 'track',
+        messageId: uuidv4(),
+        userId: userId,
+        event: eventName,
+        timestamp: now,
+        context: {
+          library: {
+            name: 'slot-machine',
+          },
+          sdkMeta: {
+            channelId
           }
         },
-        "properties": Object.assign({}, {
-          "orgId": orgId,
-          "workspaceId": workspaceId
-        }, properties)
-      }
-    ]
+        properties: Object.assign({}, properties),
+      },
+    ],
   };
 
   fetch(endpoint, {
     method: 'POST',
-    body: JSON.stringify(eventToSend)
+    headers: {
+      Authorization:
+        'Basic ' + new Buffer(sdkWriteKey + ':').toString('base64'),
+    },
+    body: JSON.stringify(eventToSend),
+  }).then((response) => {
+    console.log('status', response.status);
+    // response.text().then(text => console.log(text));
+    response.json().then(json => {
+      if (Array.isArray(json) && json.length > 0 && 'actions' in json[0] && Array.isArray(json[0].actions) && json[0].actions.length > 0) {
+        setTimeout(() => {
+          alert('Snapyr says: It\'s OK, keep trying!');
+        }, 2200);
+      }
+    });
   });
-
 };
